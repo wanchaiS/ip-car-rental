1ee39335889405d10fa79ea20ee087991aa0a8fb
wanchais <wanchai.sangkusolwong@gmail.com>
2025-05-13 23:11:25 +1000

    test log

--- src/routes/main.tsx
+++ src/routes/main.tsx
@@ -20,6 +20,7 @@ export default function Main() {
     if (cars) setFilteredCars(cars);
   }, [cars]);
 
+  console.log('here')
   // Generate search suggestions
   const searchSuggestions = (cars || []).reduce((suggestions, car) => {
     const searchTerms = [
